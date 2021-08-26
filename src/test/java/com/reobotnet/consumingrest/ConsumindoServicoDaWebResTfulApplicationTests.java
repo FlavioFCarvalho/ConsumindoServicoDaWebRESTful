5c86dd339ef637dc99d7f94db9295aaba210b7a2
FlavioFCarvalho <flaviocfr@gmail.com>
2021-08-26 16:49:34 -0300

    Open house

---
+++ src/test/java/com/reobotnet/consumingrest/ConsumindoServicoDaWebResTfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reobotnet.consumingrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumindoServicoDaWebResTfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
